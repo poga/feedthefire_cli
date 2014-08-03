79eb5f1a347df13c3080b6d470d16aefdd139ed1
Poga Po <poga.bahamut@gmail.com>
2014-08-04 01:47:23 +0800

    remove express

--- app2.js
+++ app2.js
@@ -6,7 +6,6 @@ var fs = require("fs")
   , Firebase = require("firebase")
   , express = require("express");
 
-var app = express();
 var feeds = {};
 var feedContent = {};
 
@@ -16,22 +15,6 @@ var FBURL = "torid-fire-6233.firebaseio.com";
 var SECRET = "V0Fw33XLTOaFWS9OEr8tv7srY9nEU0O9ZSwxpQjT";
 var FEEDURL="http://blog.g0v.tw/posts.atom";
 var ref = new Firebase(FBURL);
-
-app.get("/", function(req, res) {
-  fs.readFile("index.html", {encoding: 'utf8'}, function(err, data) {
-    if (err != null) {
-      res.send(500, {error: err});
-    } else {
-      res.send(data.replace("{{ FBURL }}", FBURL));
-    }
-  });
-});
-app.use("/static", express.static(__dirname + "/static"));
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Serving on port " + port);
-});
 
 ref.auth(SECRET, function(err) {
   if (err) {
